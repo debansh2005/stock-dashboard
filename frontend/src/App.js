3350ec542987416a559678ea971c235f08a53e48
debansh2005 <debansh24082005@gmail.com>
2026-04-18 13:53:14 +0530

    fix duplicate react import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { Line } from "react-chartjs-2";
